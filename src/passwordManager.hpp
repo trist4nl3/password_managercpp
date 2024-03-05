7b802233be5afc4c724cdbe55c07db43c871ab93
trist4nl3 <s4008982@student.rmit.edu.au>
2024-03-05 19:52:18 +1100

    header file for password manager

--- src/passwordManager.hpp
+++ src/passwordManager.hpp
@@ -2,4 +2,28 @@
 #include <unordered_map>
 #include <string>
 #include "password.hpp"
-#include "structs/password.hpp"
+#include "user.hpp"
+#include "encryption.hpp"
+#include "fileHandling.hpp"
+#include "userAuthentication.hpp"
+
+class PasswordManager {
+private:
+    std::vector<Password> passwords;
+    std::unordered_map<std::string, User> users;
+    Encryption encryption;
+    FileHandling fileHandling;
+    UserAuthentication userAuth;
+
+    bool userExists(const std::string& username) const;
+    bool passwordExists(const std::string& username, const std::string& password) const;
+
+public:
+    void addUser(const std::string& username, const std::string& masterPassword);
+    bool loginUser(const std::string& username, const std::string& masterPassword);
+    void addPassword(const std::string& username, const std::string& password, const std::string& serviceName);
+    std::string getPassword(const std::string& username, const std::string& serviceName) const;
+    void updatePassword(const std::string& username, const std::string& oldPassword, const std::string& newPassword);
+    void deletePassword(const std::string& username, const std::string& serviceName);
+    void changeMasterPassword(const std::string& username, const std::string& oldMasterPassword, const std::string& newMasterPassword);
+};
